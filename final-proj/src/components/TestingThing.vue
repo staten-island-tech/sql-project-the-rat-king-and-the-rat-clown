3a0e1dc635d2538ee007738a078945b691e5aa7f
RodentCoder <ichen080405@gmail.com>
2023-05-15 13:14:47 -0400

    Idk

---
+++ final-proj/src/components/TestingThing.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+</template>
+
+<script setup>
+import { supabase } from '../supabase'
+import { onMounted, ref } from 'vue'
+
+const countries = ref([])
+
+onMounted(() => {
+  yes()
+})
+const yes = async function () {
+  let { data: countries, error } = await supabase.from('countries').select('name')
+  countries.value = data
+}
+</script>
+
+<style></style>
