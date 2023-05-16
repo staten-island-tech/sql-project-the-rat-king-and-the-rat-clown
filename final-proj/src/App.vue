23db030641b650ec2686a2fd6e94a3fed081b732
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-05-16 13:14:40 -0400

    What am I doing? something!

--- final-proj/src/App.vue
+++ final-proj/src/App.vue
@@ -2,15 +2,29 @@
   <ul>
     <li v-for="profile in prof">{{ profile }}</li>
   </ul>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="session" :session="session" />
+    <Auth v-else />
+  </div>
 </template>
 
 <script setup>
 import { supabase } from './supabase'
 import { onMounted, ref } from 'vue'
+import Account from './components/Account.vue'
+import Auth from './components/Auth.vue'
 
 const prof = ref([])
+const session = ref()
 
 onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
   yes()
 })
 
--- final-proj/src/router/index.js
+++ final-proj/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import Protected from '../views/Protected.vue'
+import { userSessionStore } from '../stores/userSession'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +9,31 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/protected',
+      name: 'protected',
+      component: Protected,
+      meta: {
+        needsAuth: true
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const userSession = userSessionStore()
+
+  if (to.meta.needsAuth) {
+    if (userSession.session) {
+      return next()
+    } else {
+      return next('/')
+    }
+  }
+
+  return next()
 })
 
 export default router
