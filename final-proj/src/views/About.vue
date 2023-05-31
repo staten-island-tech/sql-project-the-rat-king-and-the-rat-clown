906c122396b25463dc01456e20bb695695f758e5
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-05-31 12:38:12 -0400

    more html yay

--- final-proj/src/views/About.vue
+++ final-proj/src/views/About.vue
@@ -1,6 +1,10 @@
 <template>
   <div v-if="userSession.session">
    You are logged in!!!!
+   <button>Click Here to Make Poll</button>
+  </div>
+  <div v-else>
+    Please log in to continue. 
   </div>
 </template>
 
