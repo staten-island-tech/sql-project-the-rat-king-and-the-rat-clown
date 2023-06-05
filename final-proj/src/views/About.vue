13e1796e6a795cc26390c6a8a7d16f0164d56f23
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-06-05 13:14:41 -0400

    started data submit form

--- final-proj/src/views/About.vue
+++ final-proj/src/views/About.vue
@@ -1,6 +1,11 @@
 <template>
   <div v-if="userSession.session">
-    
+    <form @submit.prevent="createPoll">
+    <input v-model="pollTitle" placeholder="Title">
+    <input v-model="choice1" placeholder="First Choice">
+    <input v-model="choice2" placeholder="Second choice">
+    <button>Create Poll</button>    
+  </form>
   </div>
   <div v-else>
     Please log in to continue. 
@@ -9,7 +14,19 @@
 
 <script setup>
 import { userSessionStore } from '../stores/userSession'
+import { supabase } from '../supabase'
+import { onMounted, ref } from 'vue'
 const userSession = userSessionStore()
+const pollTitle = ''
+const choice1 = ''
+const choice2 = ''
+async function createPoll(){
+  const { data, error } = await supabase
+  .from('polls')
+  .insert([
+    { title: pollTitle, choice1: choice1, choice1value: 0, choice2: choice2, choice2value: 0 },
+  ])
+}
 </script>
 
 <style lang="scss" scoped>
